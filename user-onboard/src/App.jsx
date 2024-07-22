b2ff2e87f71fad1fd7232b14acb996892aa433ba
Taimour Ali <133780034+TaimourAli23@users.noreply.github.com>
2024-07-22 12:09:09 -0700

    tailwind integration

--- user-onboard/src/App.jsx
+++ user-onboard/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +8,7 @@ function App() {
   return (
     <>
       <div>
+        <p className="text-3xl font-bold underline">Tailwind Integration</p>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
